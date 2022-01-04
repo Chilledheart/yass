6405ce970650638a49f9b63e6dd89dcc1d4973cd
Chilledheart <rwindz0@gmail.com>
2022-01-04 16:17:55 +0800

    mac: add check about univeral binary
    build script can check if the built binary and dependencies are
    built universally now.

--- scripts/build.py
+++ scripts/build.py
@@ -145,6 +145,35 @@ def get_dependencies_recursively(path):
     raise IOError('not supported in platform %s' % sys.platform)
 
 
+def check_string_output(command):
+    return subprocess.check_output(command, stderr=subprocess.STDOUT).decode().strip()
+
+
+def check_universal_build_bundle_darwin(bundle_path):
+  # export from NSBundle.h
+  NSBundleExecutableArchitectureX86_64    = 0x01000007
+  NSBundleExecutableArchitectureARM64     = 0x0100000c
+  from Foundation import NSBundle
+
+  bundle = NSBundle.bundleWithPath_(bundle_path)
+  if not bundle:
+    return False
+
+  arches = bundle.executableArchitectures()
+  if NSBundleExecutableArchitectureX86_64 in arches and NSBundleExecutableArchitectureARM64 in arches:
+    return True
+
+  return False
+
+
+def check_universal_build_dylib_darwin(lib_path):
+  arches = check_string_output(['lipo', '-archs', lib_path])
+  if 'x86_64' in arches and 'arm64' in arches:
+    return True
+
+  return False
+
+
 def _postbuild_copy_libraries_posix():
   lib_path = 'lib'
   binaries = [APP_NAME]
@@ -332,6 +361,7 @@ def postbuild_fix_retina_display():
   write_output(['plutil', '-insert', 'NSPrincipalClass', '-string', 'NSApplication', '%s/Contents/Info.plist' % get_app_name()])
   write_output(['plutil', '-insert', 'NSHighResolutionCapable', '-bool', 'YES', '%s/Contents/Info.plist' % get_app_name()])
 
+
 def postbuild_codesign():
   print 'fixing codesign...'
   # reference https://developer.apple.com/documentation/security/notarizing_macos_software_before_distribution/resolving_common_notarization_issues?language=objc
@@ -339,6 +369,64 @@ def postbuild_codesign():
   write_output(['codesign', '--timestamp=none', '--preserve-metadata=entitlements', '--options=runtime', '--force', '--deep', '--sign', DEFAULT_SIGNING_IDENTITY, get_app_name()])
   write_output(['codesign', '-dv', '--deep', '--strict', '--verbose=4', get_app_name()])
   write_output(['codesign', '-d', '--entitlements', ':-', get_app_name()])
+
+
+def _check_universal_build_darwin(path, verbose = False):
+  checked = True
+  status_msg = ''
+  error_msg = ''
+
+  if path.endswith('.framework') or path.endswith('.app') or path.endswith('.appex'):
+    if not check_universal_build_bundle_darwin(path):
+      status_msg += 'bundle "%s" is not universal built\n' % path
+      error_msg += 'bundle "%s" is not universal built\n' % path
+      checked = False
+    else:
+      status_msg += 'bundle "%s" is universal built\n' % path
+
+    for root, dirs, files in os.walk(path):
+      for d in dirs:
+        full_path = os.path.join(root, d)
+        if full_path.endswith('.framework') or full_path.endswith('.app') or full_path.endswith('.appex'):
+          if not check_universal_build_bundle_darwin(full_path):
+            error_msg += 'bundle "%s" is not universal built\n' % full_path
+            status_msg += 'bundle "%s" is not universal built\n' % full_path
+            checked = False
+          else:
+            status_msg += 'bundle "%s" is universal built\n' % full_path
+
+      for f in files:
+        full_path = os.path.join(root, f)
+        if full_path.endswith('.dylib') or full_path.endswith('.so'):
+          if not check_universal_build_dylib_darwin(full_path):
+            error_msg += 'dylib "%s" is not universal built\n' % full_path
+            status_msg += 'dylib "%s" is not universal built\n' % full_path
+            checked = False
+          else:
+            status_msg += 'dylib "%s" is universal built\n' % full_path
+
+  if path.endswith('.dylib') or path.endswith('.so'):
+    if not check_universal_build_dylib_darwin(path):
+      error_msg += 'dylib "%s" is not universal built\n' % path
+      status_msg += 'dylib "%s" is not universal built\n' % path
+      checked = False
+    else:
+      status_msg += 'dylib "%s" is universal built\n' % path
+
+
+  if verbose:
+    print status_msg
+
+  if not checked:
+    raise IOError(error_msg)
+
+
+def postbuild_check_universal_build():
+    print 'check universal build...'
+    # check if binary is built universally
+    if sys.platform == 'darwin':
+        _check_universal_build_darwin(get_app_name(), verbose = True)
+
 
 def postbuild_archive():
   if sys.platform == 'win32':
@@ -390,6 +478,7 @@ if __name__ == '__main__':
   if sys.platform == 'darwin':
     postbuild_fix_retina_display()
     postbuild_codesign()
+  postbuild_check_universal_build()
   outputs = postbuild_archive()
   for output in outputs:
     print '------ %s' % output
