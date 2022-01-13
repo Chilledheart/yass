314b310cc712c966f1e053d09d652b112454f17d
Chilledheart <rwindz0@gmail.com>
2022-01-13 20:36:56 +0800

    attempt to fix ci bots

--- scripts/build.bat
+++ scripts/build.bat
@@ -20,6 +20,30 @@ set CC=
 set CXX=
 set Platform=x86
 
+cd third_party\boringssh
+mkdir "%Platform%"
+mkdir "%Platform%\debug"
+
+if not exist "%Platform%\debug\crypto.lib" (
+  mkdir build
+  cd build
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug ..
+  ninja crypto
+  copy /y crypto\crypto.lib "..\%Platform%\debug\crypto.lib"
+  cd ..
+  rmdir build /s /q
+)
+
+if not exist "%Platform%\crypto.lib" (
+  mkdir build
+  cd build
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release ..
+  ninja crypto
+  copy /y crypto\crypto.lib "..\%Platform%\crypto.lib"
+  cd ..
+  rmdir build /s /q
+)
+
 python.exe -u .\scripts\build.py || exit /b
 
 set "VSCMD_START_DIR=%CD%"
@@ -28,6 +52,30 @@ set CC=
 set CXX=
 set Platform=x64
 
+cd third_party\boringssh
+mkdir "%Platform%"
+mkdir "%Platform%\debug"
+
+if not exist "%Platform%\debug\crypto.lib" (
+  mkdir build
+  cd build
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug ..
+  ninja crypto
+  copy /y crypto\crypto.lib "..\%Platform%\debug\crypto.lib"
+  cd ..
+  rmdir build /s /q
+)
+
+if not exist "%Platform%\crypto.lib" (
+  mkdir build
+  cd build
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release ..
+  ninja crypto
+  copy /y crypto\crypto.lib "..\%Platform%\crypto.lib"
+  cd ..
+  rmdir build /s /q
+)
+
 python.exe -u .\scripts\build.py || exit /b
 
 set "VSCMD_START_DIR=%CD%"
@@ -35,5 +83,29 @@ call "%vsdevcmd%" -arch=arm64 -host_arch=amd64
 set CC=
 set CXX=
 set Platform=arm64
+
+cd third_party\boringssh
+mkdir "%Platform%"
+mkdir "%Platform%\debug"
+
+if not exist "%Platform%\debug\crypto.lib" (
+  mkdir build
+  cd build
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Debug ..
+  ninja crypto
+  copy /y crypto\crypto.lib "..\%Platform%\debug\crypto.lib"
+  cd ..
+  rmdir build /s /q
+)
+
+if not exist "%Platform%\crypto.lib" (
+  mkdir build
+  cd build
+  cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release ..
+  ninja crypto
+  copy /y crypto\crypto.lib "..\%Platform%\crypto.lib"
+  cd ..
+  rmdir build /s /q
+)
 
 python.exe -u .\scripts\build.py || exit /b
