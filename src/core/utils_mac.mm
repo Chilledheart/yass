65183a88721b9c1ee70bf0bc206b4d23028b897c
Chilledheart <rwindz0@gmail.com>
2022-01-31 02:17:24 +0800

    core: implement SetThreadName and SetThreadPriority

--- src/core/utils_mac.mm
+++ src/core/utils_mac.mm
@@ -10,9 +10,51 @@
 
 #include <mach/mach_time.h>
 #include <stdint.h>
+#include <pthread.h>
 
 #include "core/logging.hpp"
 #include "core/foundation_util.hpp"
+
+bool SetThreadPriority(std::thread::native_handle_type handle,
+                       ThreadPriority priority) {
+  // Changing the priority of the main thread causes performance regressions.
+  // https://crbug.com/601270
+  DCHECK(!pthread_main_np());
+  qos_class_t qos_class = QOS_CLASS_UNSPECIFIED;
+  int relative_priority = 0; // Undocumented
+
+  switch (priority) {
+    case ThreadPriority::BACKGROUND:
+      qos_class = QOS_CLASS_BACKGROUND;
+      relative_priority = 0;
+      break;
+    case ThreadPriority::NORMAL:
+      qos_class = QOS_CLASS_UTILITY;
+      relative_priority = 0;
+      break;
+    case ThreadPriority::ABOVE_NORMAL: {
+      qos_class = QOS_CLASS_USER_INITIATED;
+      relative_priority = 0;
+      break;
+    }
+    case ThreadPriority::TIME_CRITICAL:
+      qos_class = QOS_CLASS_USER_INTERACTIVE;
+      relative_priority = 0;
+      break;
+  }
+  /// documented in
+  /// https://developer.apple.com/documentation/apple-silicon/tuning-your-code-s-performance-for-apple-silicon
+  /// and https://developer.apple.com/library/archive/documentation/Performance/Conceptual/EnergyGuide-iOS/PrioritizeWorkWithQoS.html
+  return pthread_set_qos_class_self_np(qos_class, relative_priority) == 0;
+
+}
+
+bool SetThreadName(std::thread::native_handle_type handle,
+                   const std::string& name) {
+  // pthread_setname() fails (harmlessly) in the sandbox, ignore when it does.
+  // See http://crbug.com/47058
+  return pthread_setname_np(name.c_str()) == 0;
+}
 
 static uint64_t MachTimeToNanoseconds(uint64_t machTime) {
   uint64_t nanoseconds = 0;
