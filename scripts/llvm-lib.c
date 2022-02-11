74330482c057716a6d7e35e65413cb26fb0b9f31
Chilledheart <rwindz0@gmail.com>
2022-02-11 16:42:46 +0800

    win32: llvm-lib can handle with keyboard interrupt

--- scripts/llvm-lib.c
+++ scripts/llvm-lib.c
@@ -31,6 +31,19 @@ int wmain(int argc, wchar_t *argv[]) {
   return 0;
 }
 #else
+static HANDLE hProcess;
+static BOOL WINAPI NotifyInterrupted(DWORD dwCtrlType) {
+  if (dwCtrlType == CTRL_C_EVENT || dwCtrlType == CTRL_BREAK_EVENT) {
+    /* deal with subprocess */
+    if (!GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, GetProcessId(hProcess))) {
+      fprintf(stderr, "Failed to send CTRL_BREAK_EVENT to subprocess due to error: %ld\n",
+              GetLastError());
+      return FALSE;
+    }
+    return TRUE;
+  }
+  return FALSE;
+}
 // It works
 int wmain() {
   // Generate exe_path (aka lld-link.exe)
@@ -42,9 +55,12 @@ int wmain() {
   wchar_t *cmdline = GetCommandLineW();
   int argc;
   wchar_t** argv = CommandLineToArgvW(cmdline, &argc);
-  memcpy(wcsrchr(argv[0], '\\') + 1, L"lld-link.exe", sizeof(L"lld-link.exe"));
   const wchar_t** shim_argv = (const wchar_t**)malloc((argc + 2) * sizeof(wchar_t*));
-  shim_argv[0] = argv[0];
+  if (!shim_argv) {
+    fprintf(stderr, "Out of memory\n");
+    return -1;
+  }
+  shim_argv[0] = exe_path;
   shim_argv[1] = L"/lib";
   memcpy(shim_argv + 2, argv + 1, sizeof(wchar_t*) * (argc - 1));
   shim_argv[argc + 1] = L"\0";
@@ -52,6 +68,10 @@ int wmain() {
   // Convert shim_argv to shim_cmdline
   wchar_t* shim_cmdline =
       (wchar_t*)malloc(UNICODE_STRING_MAX_CHARS /*32767*/ * sizeof(wchar_t));
+  if (!shim_cmdline) {
+    fprintf(stderr, "Out of memory\n");
+    return -1;
+  }
   wchar_t *pos = shim_cmdline;
   for (int i = 0; i < argc + 1; ++i) {
     memcpy(pos, L"\"", sizeof(wchar_t));
@@ -68,6 +88,7 @@ int wmain() {
   }
   *pos = L'\0';
 
+  // Start process
   STARTUPINFOW startupInfo = {};
   startupInfo.cb = sizeof(STARTUPINFO);
   startupInfo.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
@@ -77,7 +98,7 @@ int wmain() {
 
   PROCESS_INFORMATION processInformation = {};
 
-  if (!CreateProcessW(exe_path, shim_cmdline, NULL, NULL, FALSE,
+  if (!CreateProcessW(exe_path, shim_cmdline, NULL, NULL, TRUE,
                       NORMAL_PRIORITY_CLASS, NULL, NULL, &startupInfo,
                       &processInformation)) {
     fprintf(stderr, "Failed to execute cmd %ls due to error: %ld\n",
@@ -85,11 +106,46 @@ int wmain() {
     return -1;
   }
 
-  // Let's wait for the process to finish.
-  WaitForSingleObject(processInformation.hProcess, INFINITE);
+  hProcess = processInformation.hProcess;
 
   DWORD dw_exit_code;
-  GetExitCodeProcess(processInformation.hProcess, &dw_exit_code);
+
+  if (!GetExitCodeProcess(processInformation.hProcess, &dw_exit_code)) {
+    fprintf(stderr, "Failed to get subprocess exit code due to error: %ld\n",
+            GetLastError());
+    return -1;
+  }
+
+  // Let's wait for the process to finish.
+  if (dw_exit_code == STILL_ACTIVE) {
+    // Handle with interrupt
+    if (!SetConsoleCtrlHandler(NotifyInterrupted, TRUE)) {
+      fprintf(stderr, "Failed to register interrupt handler due to error: %ld\n",
+              GetLastError());
+      return -1;
+    }
+
+    if (WaitForSingleObject(processInformation.hProcess, INFINITE) != WAIT_OBJECT_0) {
+      fprintf(stderr, "Failed to wait subprocess due to error: %ld\n",
+              GetLastError());
+      return -1;
+    }
+
+    // Reset Handle with interrupt
+    SetConsoleCtrlHandler(NotifyInterrupted, FALSE);
+
+    if (!GetExitCodeProcess(processInformation.hProcess, &dw_exit_code)) {
+      fprintf(stderr, "Failed to get subprocess exit code due to error: %ld\n",
+              GetLastError());
+      return -1;
+    }
+  }
+
+  // Unlikely code path
+  if (dw_exit_code == STILL_ACTIVE) {
+    fprintf(stderr, "Killing pending subprocess\n");
+    TerminateProcess(processInformation.hProcess, 1);
+  }
 
   CloseHandle(processInformation.hThread);
   CloseHandle(processInformation.hProcess);
