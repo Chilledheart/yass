5ff6cbd25c961a1865717701cbaff61f024a0f0f
Chilledheart <rwindz0@gmail.com>
2022-03-11 17:01:32 +0800

    fix windows build

--- scripts/download-toolchain.bat
+++ scripts/download-toolchain.bat
@@ -11,7 +11,7 @@ echo "Install dependency: prebuilt nasm"
 
 cd third_party
 curl -O https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win64/nasm-2.15.05-win64.zip
-"C:\Program Files\7-Zip\7z.exe" x nasm-2.15.05-win64.zip
+"C:\Program Files\7-Zip\7z.exe" x nasm-2.15.05-win64.zip -aoa
 rename nasm-2.15.05 nasm
 cd ..
 
@@ -22,4 +22,4 @@ python -u scripts\download-clang-prebuilt-binaries.py
 echo "Install dependency: wixtoolset 3"
 
 curl -L -O https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311-binaries.zip
-"C:\Program Files\7-Zip\7z.exe" x "-othird_party\wix311" wix311-binaries.zip
+"C:\Program Files\7-Zip\7z.exe" x "-othird_party\wix311" wix311-binaries.zip -aoa
