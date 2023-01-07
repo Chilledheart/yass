074ab637365d774fa74af3460ea193077271a104
Chilledheart <rwindz0@gmail.com>
2023-01-07 18:22:26 +0800

    network: Implement tcp_keep_alive options (gui, part3)

--- src/mac/OptionViewController.mm
+++ src/mac/OptionViewController.mm
@@ -22,6 +22,12 @@
   self.tcpSoLingerTimeout.intValue = absl::GetFlag(FLAGS_so_linger_timeout);
   self.tcpSendBuffer.intValue = absl::GetFlag(FLAGS_so_snd_buffer);
   self.tcpRecevieBuffer.intValue = absl::GetFlag(FLAGS_so_rcv_buffer);
+
+  [self.tcpKeepAlive
+      setState:(absl::GetFlag(FLAGS_tcp_keep_alive) ? NSControlStateValueOn : NSControlStateValueOff)];
+  self.tcpKeepAliveCnt.intValue = absl::GetFlag(FLAGS_tcp_keep_alive_cnt);
+  self.tcpKeepAliveIdleTimeout.intValue = absl::GetFlag(FLAGS_tcp_keep_alive_idle_timeout);
+  self.tcpKeepAliveInterval.intValue = absl::GetFlag(FLAGS_tcp_keep_alive_interval);
 }
 
 - (void)viewDidDisappear {
@@ -34,6 +40,11 @@
   absl::SetFlag(&FLAGS_so_linger_timeout, self.tcpSoLingerTimeout.intValue);
   absl::SetFlag(&FLAGS_so_snd_buffer, self.tcpSendBuffer.intValue);
   absl::SetFlag(&FLAGS_so_rcv_buffer, self.tcpRecevieBuffer.intValue);
+
+  absl::SetFlag(&FLAGS_tcp_keep_alive, self.tcpKeepAlive.state == NSControlStateValueOn);
+  absl::SetFlag(&FLAGS_tcp_keep_alive_cnt, self.tcpKeepAliveCnt.intValue);
+  absl::SetFlag(&FLAGS_tcp_keep_alive_idle_timeout, self.tcpKeepAliveIdleTimeout.intValue);
+  absl::SetFlag(&FLAGS_tcp_keep_alive_interval, self.tcpKeepAliveInterval.intValue);
   [self dismissViewController:self];
 }
 
