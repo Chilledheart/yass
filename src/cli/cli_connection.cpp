53979cf9cf4eb586e3a692f2769f829492b4e74a
Chilledheart <hukeyue@hotmail.com>
2023-02-04 18:00:32 +0800

    network: send http2 settings (default)

--- src/cli/cli_connection.cpp
+++ src/cli/cli_connection.cpp
@@ -16,12 +16,41 @@
 
 namespace cli {
 
+// from net/http/http_network_session.h
+// Specifies the maximum HPACK dynamic table size the server is allowed to set.
+const uint32_t kSpdyMaxHeaderTableSize = 64 * 1024;
+
+// The maximum size of header list that the server is allowed to send.
+const uint32_t kSpdyMaxHeaderListSize = 256 * 1024;
+
+// Specifies the maximum concurrent streams server could send (via push).
+const uint32_t kSpdyMaxConcurrentPushedStreams = 1000;
+
+// Specifies the the default value for the push setting, which is disabled.
+const uint32_t kSpdyDisablePush = 0;
+
+// from net/http/http_network_session.cc
+// The maximum receive window sizes for HTTP/2 sessions and streams.
+const int32_t kSpdySessionMaxRecvWindowSize = 15 * 1024 * 1024;  // 15 MB
+const int32_t kSpdyStreamMaxRecvWindowSize = 6 * 1024 * 1024;    //  6 MB
+
 namespace {
 // from spdy_session.h
 // If more than this many bytes have been read or more than that many
 // milliseconds have passed, return ERR_IO_PENDING from ReadLoop.
 const int kYieldAfterBytesRead = 32 * 1024;
 const int kYieldAfterDurationMilliseconds = 20;
+
+// from net/spdy/spdy_session.h
+// Maximum number of capped frames that can be queued at any time.
+// We measured how many queued capped frames were ever in the
+// SpdyWriteQueue at one given time between 2019-08 and 2020-02.
+// The numbers showed that in 99.94% of cases it would always
+// stay below 10, and that it would exceed 1000 only in
+// 10^-8 of cases. Therefore we picked 10000 as a number that will
+// virtually never be hit in practice, while still preventing an
+// attacker from growing this queue unboundedly.
+const int kSpdySessionMaxQueuedCappedFrames = 10000;
 
 std::vector<http2::adapter::Header> GenerateHeaders(
   std::vector<std::pair<std::string, std::string>> headers, int status = 0) {
@@ -1380,6 +1409,24 @@ void CliConnection::connected() {
     decoder_ = std::make_unique<cipher>("", absl::GetFlag(FLAGS_password),
       static_cast<enum cipher_method>(absl::GetFlag(FLAGS_cipher_method)),
       this);
+  }
+
+  // Send Upstream Settings (HTTP2 Only)
+  if (adapter_) {
+    std::vector<http2::adapter::Http2Setting> settings {
+      { http2::adapter::Http2KnownSettingsId::HEADER_TABLE_SIZE,
+        kSpdyMaxHeaderTableSize },
+      { http2::adapter::Http2KnownSettingsId::MAX_CONCURRENT_STREAMS,
+        kSpdyMaxConcurrentPushedStreams },
+      { http2::adapter::Http2KnownSettingsId::INITIAL_WINDOW_SIZE,
+        kSpdyStreamMaxRecvWindowSize },
+      { http2::adapter::Http2KnownSettingsId::MAX_HEADER_LIST_SIZE,
+        kSpdyMaxHeaderListSize },
+      { http2::adapter::Http2KnownSettingsId::ENABLE_PUSH,
+        kSpdyDisablePush },
+    };
+    adapter_->SubmitSettings(settings);
+    SendIfNotProcessing();
   }
 
   // Send Upstream Header
