9c097dfaac361234bcc2a61c1ec5271658e476eb
Chilledheart <hukeyue@hotmail.com>
2023-06-09 11:34:03 +0800

    core: fix asio tracking build

--- src/core/asio.cpp
+++ src/core/asio.cpp
@@ -14,6 +14,7 @@
 #pragma GCC diagnostic ignored "-Wshadow"
 #pragma GCC diagnostic ignored "-Wsign-compare"
 #pragma GCC diagnostic ignored "-Wformat-nonliteral"
+#pragma GCC diagnostic ignored "-Wunused-result"
 // new in GCC 7, see
 // https://developers.redhat.com/blog/2017/03/10/wimplicit-fallthrough-in-gcc-7
 #if (defined(__GNUC__) && (__GNUC__ >= 7)) || defined(__clang__)
