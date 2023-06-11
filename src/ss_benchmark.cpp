4d64076d258a34cb13728789ad15eca163744f59
Chilledheart <hukeyue@hotmail.com>
2023-06-11 19:31:48 +0800

    test: increase tcp connect timeout

--- src/ss_benchmark.cpp
+++ src/ss_benchmark.cpp
@@ -498,6 +498,7 @@ int main(int argc, char** argv) {
 
   absl::SetFlag(&FLAGS_v, 0);
   absl::SetFlag(&FLAGS_log_thread_id, 1);
+  absl::SetFlag(&FLAGS_connect_timeout, 500);
   absl::SetFlag(&FLAGS_tcp_user_timeout, 1000);
 
   ::CRYPTO_library_init();
