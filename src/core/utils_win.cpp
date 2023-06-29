78e5643fcdc98b68277a47e9abfe02c17d63bc16
Chilledheart <hukeyue@hotmail.com>
2023-06-29 18:19:49 +0800

    Fix another typo in utils_win.cpp

--- src/core/utils_win.cpp
+++ src/core/utils_win.cpp
@@ -784,7 +784,7 @@ void SetExecutablePath(const std::string& exe_path) {
   main_exe_path = exe_path;
 
   std::string new_exe_path;
-  GetExecutablePath(new_exe_path);
+  GetExecutablePath(&new_exe_path);
   absl::flags_internal::SetProgramInvocationName(new_exe_path);
 }
 
