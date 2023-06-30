79469af6cd23986067ef24f7255da24c415df8fe
Chilledheart <hukeyue@hotmail.com>
2023-06-30 10:50:58 +0800

    http2: handle end stream event more gracefully
    GoAway frames should be sent.

--- src/server/server_connection.cpp
+++ src/server/server_connection.cpp
@@ -166,7 +166,8 @@ void ServerConnection::close() {
       data_frame_ = nullptr;
       stream_id_ = 0;
     }
-    adapter_->SubmitShutdownNotice();
+    adapter_->SubmitGoAway(0, http2::adapter::Http2ErrorCode::HTTP2_NO_ERROR, "");
+    DCHECK(adapter_->want_write());
     SendIfNotProcessing();
     WriteStreamInPipe();
   }
@@ -307,7 +308,7 @@ bool ServerConnection::OnEndHeadersForStream(
       << " Unexpected auth token.";
     return false;
   }
-  auto padding_support = request_map_.find("padding") != request_map_.end();
+  bool padding_support = request_map_.find("padding") != request_map_.end();
   asio::error_code ec;
   auto peer_endpoint = socket_.remote_endpoint(ec);
   if (padding_support_ && padding_support) {
@@ -339,7 +340,10 @@ bool ServerConnection::OnEndStream(StreamId stream_id) {
   if (stream_id == stream_id_) {
     data_frame_ = nullptr;
     stream_id_ = 0;
-    OnDisconnect(asio::error::eof);
+    adapter_->SubmitGoAway(0, http2::adapter::Http2ErrorCode::HTTP2_NO_ERROR, "");
+    DCHECK(adapter_->want_write());
+    SendIfNotProcessing();
+    WriteStreamInPipe();
   }
   return true;
 }
@@ -430,7 +434,7 @@ void ServerConnection::OnRstStream(StreamId stream_id,
 bool ServerConnection::OnGoAway(StreamId last_accepted_stream_id,
                                 http2::adapter::Http2ErrorCode error_code,
                                 absl::string_view opaque_data) {
-  OnDisconnect(asio::error::connection_reset);
+  OnDisconnect(asio::error::eof);
   return true;
 }
 
