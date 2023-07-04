6dceb979e665e7a4dea004f5fd21d887419fab73
Chilledheart <hukeyue@hotmail.com>
2023-07-04 11:30:17 +0800

    core: hide embedded binary symbols

--- third_party/ca-certificates/ca-bundle.crt.apple.s
+++ third_party/ca-certificates/ca-bundle.crt.apple.s
@@ -1,5 +1,7 @@
 .section __DATA_CONST,__const
+.private_extern __binary_ca_bundle_crt_start
 .global __binary_ca_bundle_crt_start
+.private_extern __binary_ca_bundle_crt_end
 .global __binary_ca_bundle_crt_end
 .p2align  6
 
