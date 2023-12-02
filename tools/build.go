5614cf6652ae81cd5a08da2a7a4df246f101e7a9
Keeyou <keeyou-cn@outlook.com>
2023-12-02 14:49:34 +0800

    icu: copy icudata file in packaging

--- tools/build.go
+++ tools/build.go
@@ -1155,7 +1155,7 @@ func archiveMainFile(output string, prefix string, paths []string) {
 	}
 }
 
-func generateMsi(output string, dllPaths []string, licensePaths []string, hasCrashpad bool) {
+func generateMsi(output string, dllPaths []string, licensePaths []string, hasCrashpad bool, hasIcuDataFile bool) {
 	wxsTemplate, err := ioutil.ReadFile(filepath.Join("..", "yass.wxs"))
 	if err != nil {
 		glog.Fatalf("%v", err)
@@ -1175,6 +1175,9 @@ func generateMsi(output string, dllPaths []string, licensePaths []string, hasCra
 	wxsXml = strings.Replace(wxsXml, "<!-- %LICENSEPLACEHOLDER% -->", licenseReplacement, 1)
 	if (hasCrashpad) {
 		wxsXml = strings.Replace(wxsXml, "<!-- %CRASHPAD_HANDLER_HOLDER% -->", "<File Name='crashpad_handler.exe' Source='crashpad_handler.exe' KeyPath='no'/>", 1)
+	}
+	if (hasIcuDataFile) {
+		wxsXml = strings.Replace(wxsXml, "<!-- %ICUDATAFILE_HOLDER% -->", "<File Name='icudtl.dat' Source='icudtl.dat' KeyPath='no'/>", 1)
 	}
 
 	err = ioutil.WriteFile("yass.wxs", []byte(wxsXml), 0666)
@@ -1268,6 +1271,10 @@ func postStateArchives() map[string][]string {
 	if _, err := os.Stat("crashpad_handler.exe"); errors.Is(err, os.ErrNotExist) {
 		hasCrashpad = false
 	}
+	hasIcuDataFile := true
+	if _, err := os.Stat("icudtl.dat"); errors.Is(err, os.ErrNotExist) {
+		hasIcuDataFile = false
+	}
 
 	msiArchive := fmt.Sprintf(archiveFormat, APPNAME, "", ".msi")
 	nsisArchive := fmt.Sprintf(archiveFormat, APPNAME, "-installer", ".exe")
@@ -1288,6 +1295,10 @@ func postStateArchives() map[string][]string {
 	if hasCrashpad {
 		paths = append(paths, "crashpad_handler.exe")
 	}
+	// copying dependent icudata
+	if hasIcuDataFile {
+		paths = append(paths, "icudtl.dat")
+	}
 
 	// copying dependent LICENSEs
 	licensePaths := postStateArchiveLicenses()
@@ -1303,7 +1314,7 @@ func postStateArchives() map[string][]string {
 	// error CNDL0265 : The Platform attribute has an invalid value arm64.
 	// Possible values are x86, x64, or ia64.
 	if systemNameFlag == "windows" && msvcTargetArchFlag != "arm64" {
-		generateMsi(msiArchive, dllPaths, licensePaths, hasCrashpad)
+		generateMsi(msiArchive, dllPaths, licensePaths, hasCrashpad, hasIcuDataFile)
 		archives[msiArchive] = []string{msiArchive}
 	}
 	// nsis installer
