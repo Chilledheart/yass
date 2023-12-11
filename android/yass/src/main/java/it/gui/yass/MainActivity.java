b120a0332927afd170f7a970d0a7439f5a23ddf9
Keeyou <keeyou-cn@outlook.com>
2023-12-11 13:41:51 +0800

    android: add MainActivity

---
+++ android/yass/src/main/java/it/gui/yass/MainActivity.java
@@ -0,0 +1,25 @@
+package it.gui.yass;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import it.gui.yass.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
