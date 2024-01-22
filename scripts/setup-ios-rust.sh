e2283f63ff1930c07b54731fd631bdd1d64e28ad
Keeyou <keeyou-cn@outlook.com>
2024-01-22 13:49:59 +0800

    rust: pin to 1.75.0 release

--- scripts/setup-ios-rust.sh
+++ scripts/setup-ios-rust.sh
@@ -7,6 +7,13 @@ if [ -z "$(which rustup)" ]; then
   exit -1
 fi
 
+echo "Adding rustup toolchain..."
+
+rustup toolchain install 1.75.0
+rustup default 1.75.0
+
+echo "Adding rustup toolchain...done"
+
 echo "Adding rustup ios target..."
 
 rustup target add aarch64-apple-ios aarch64-apple-ios-sim x86_64-apple-ios
