861539a585ba7e17152238b96db32c2533d20d86
Keeyou <keeyou-cn@outlook.com>
2024-01-23 11:08:55 +0800

    ssl: use X509_parse_from_buffer

--- src/net/x509_util.hpp
+++ src/net/x509_util.hpp
@@ -4,6 +4,7 @@
 #ifndef H_NET_X509_UTIL
 #define H_NET_X509_UTIL
 
+#include <string_view>
 #include <openssl/base.h>
 #include <openssl/pool.h>
 
@@ -11,6 +12,8 @@ namespace net::x509_util {
 
 // Returns a CRYPTO_BUFFER_POOL for deduplicating certificates.
 CRYPTO_BUFFER_POOL* GetBufferPool();
+
+bssl::UniquePtr<CRYPTO_BUFFER> CreateCryptoBuffer(std::string_view data);
 
 } // namespace net::x509_util
 
