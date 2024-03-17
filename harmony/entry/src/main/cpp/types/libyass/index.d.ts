ee16313ea83b37be60d4bb42194ae108cfb52181
Keeyou <keeyou-cn@outlook.com>
2024-03-17 15:42:56 +0800

    harmony: add doh support

--- harmony/entry/src/main/cpp/types/libyass/index.d.ts
+++ harmony/entry/src/main/cpp/types/libyass/index.d.ts
@@ -6,10 +6,12 @@ export const getUsername: () => string;
 export const getPassword: () => string;
 export const getCipher: () => string;
 export const getCipherStrings: () => string[];
+export const getDoHUrl: () => string;
 export const getTimeout: () => number;
 export const init: (temp_dir: string, data_dir: string) => void;
 export const destroy: () => void;
 export const startWorker: (cb: (err_msg: string, port: number) => void) => void;
 export const stopWorker: (cb: () => void) => void;
 export const saveConfig: (server_host: string, server_sni: string, server_port: string,
-                          username: string, password: string, cipher: string, timeout: string) => string;
+                          username: string, password: string, cipher: string, doh_url: string,
+                          timeout: string) => string;
