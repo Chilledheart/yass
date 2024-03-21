ff691506096c3df5b70644d4ae5633b3849b7672
Keeyou <keeyou-cn@outlook.com>
2024-03-21 13:32:04 +0800

    doh: handle localhost as special domain name
    implement rfc 6761 section 6.3

--- src/net/doh_resolver_test.cpp
+++ src/net/doh_resolver_test.cpp
@@ -8,6 +8,7 @@
 
 #include <gmock/gmock.h>
 
+#include "config/config.hpp"
 #include "net/doh_resolver.hpp"
 #include "test_util.hpp"
 
@@ -18,6 +19,47 @@ ABSL_FLAG(bool, no_doh_tests, false, "skip doh tests");
 #endif
 
 using namespace net;
+
+static void DoLocalResolve(asio::io_context& io_context, scoped_refptr<DoHResolver> resolver) {
+  auto work_guard =
+      std::make_unique<asio::executor_work_guard<asio::io_context::executor_type>>(io_context.get_executor());
+
+  io_context.restart();
+
+  asio::post(io_context, [&]() {
+    resolver->AsyncResolve("doh-test.localhost", 80, [&](asio::error_code ec, asio::ip::tcp::resolver::results_type results) {
+      work_guard.reset();
+      ASSERT_FALSE(ec) << ec;
+      bool has_ipv6 = false;
+      for (auto iter = std::begin(results); iter != std::end(results); ++iter) {
+        const asio::ip::tcp::endpoint& endpoint = *iter;
+        auto addr = endpoint.address();
+        EXPECT_TRUE(addr.is_loopback()) << addr;
+        has_ipv6 |= addr.is_v6();
+      }
+      if (absl::GetFlag(FLAGS_ipv6_mode)) {
+        EXPECT_TRUE(has_ipv6) << "Expected IPv6 addresses on IPv6 mode";
+      }
+    });
+  });
+
+  EXPECT_NO_FATAL_FAILURE(io_context.run());
+}
+
+TEST(DOH_TEST, LocalBasic) {
+  if (absl::GetFlag(FLAGS_no_doh_tests)) {
+    GTEST_SKIP() << "skipped as required";
+    return;
+  }
+  asio::error_code ec;
+  asio::io_context io_context;
+
+  auto resolver = DoHResolver::Create(io_context);
+  int ret = resolver->Init("https://1.1.1.1/dns-query", 5000);
+  ASSERT_EQ(ret, 0);
+
+  DoLocalResolve(io_context, resolver);
+}
 
 static void DoRemoteResolve(asio::io_context& io_context, scoped_refptr<DoHResolver> resolver) {
   auto work_guard =
