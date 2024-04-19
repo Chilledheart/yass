5457181a7995f392d9438b98a5c24166dfe11c32
Keeyou <keeyou-cn@outlook.com>
2024-04-19 15:13:54 +0800

    quiche: update quiche_command_line_flags_impl.h

--- third_party/quiche/overrides/quiche_platform_impl/quiche_command_line_flags_impl.cc
+++ third_party/quiche/overrides/quiche_platform_impl/quiche_command_line_flags_impl.cc
@@ -5,16 +5,24 @@
 #include "quiche_platform_impl/quiche_command_line_flags_impl.h"
 
 #include <absl/flags/parse.h>
+#include <absl/flags/usage.h>
 
 std::vector<std::string> QuicheParseCommandLineFlagsImpl(
     const char* usage,
     int argc,
     const char* const* argv) {
   static_cast<void>(usage);
-  auto args = absl::ParseCommandLine(argc, const_cast<char**>(argv));
-  std::vector<std::string> ret;
-  for (const char* arg : args)
-    ret.push_back(arg);
+  auto parsed = absl::ParseCommandLine(argc, const_cast<char**>(argv));
+  std::vector<std::string> result;
+  result.reserve(parsed.size());
+  // Remove the first argument, which is the name of the binary.
+  for (size_t i = 1; i < parsed.size(); i++) {
+    result.push_back(std::string(parsed[i]));
+  }
+  return result;
+}
 
-  return ret;
+void QuichePrintCommandLineFlagHelpImpl(const char* usage) {
+  static_cast<void>(usage);
+  std::cerr << absl::ProgramUsageMessage() << std::endl;
 }
--- third_party/quiche/overrides/quiche_platform_impl/quiche_command_line_flags_impl.h
+++ third_party/quiche/overrides/quiche_platform_impl/quiche_command_line_flags_impl.h
@@ -14,11 +14,26 @@
 // ------------------------------------------------------------------------
 
 #define DEFINE_QUICHE_COMMAND_LINE_FLAG_IMPL(type, name, default_value, help) \
-  ABSL_FLAG(type, FLAGS_##name, default_value, help)
+  ABSL_FLAG(type, name, default_value, help)
+
+namespace quiche {
+
+template <typename T>
+T GetQuicheCommandLineFlag(const absl::Flag<T>& flag) {
+  return absl::GetFlag(flag);
+}
 
 std::vector<std::string> QuicheParseCommandLineFlagsImpl(
-    const char* usage,
-    int argc,
-    const char* const* argv);
+    const char* usage, int argc, const char* const* argv,
+    bool parse_only = false);
 
-#endif
+}  // namespace quiche
+
+void QuichePrintCommandLineFlagHelpImpl(const char* usage);
+
+template <typename T>
+T GetQuicheFlagImplImpl(const absl::Flag<T>& flag) {
+  return absl::GetFlag(flag);
+}
+
+#endif  // NET_THIRD_PARTY_QUICHE_OVERRIDES_QUICHE_PLATFORM_IMPL_QUICHE_COMMAND_LINE_FLAGS_IMPL_H_
