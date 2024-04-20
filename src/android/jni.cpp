c2eb338aa49dcbb6a37382e335ea1802c65e832f
Keeyou <keeyou-cn@outlook.com>
2024-04-20 08:06:00 +0800

    android: logging to stderr

--- src/android/jni.cpp
+++ src/android/jni.cpp
@@ -18,6 +18,8 @@ jobject g_activity_obj = nullptr;
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
   g_jvm = vm;
+  absl::SetFlag(&FLAGS_logtostderr, true);
+
   return JNI_VERSION_1_6;
 }
 
