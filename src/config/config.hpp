a54025ba12c027239efe6f5debd9f2b12acda194
Keeyou <keeyou-cn@outlook.com>
2024-04-22 13:09:11 +0800

    config: split config_core (trivial)

--- src/config/config.hpp
+++ src/config/config.hpp
@@ -4,45 +4,15 @@
 #ifndef H_CONFIG_CONFIG
 #define H_CONFIG_CONFIG
 
-#include <absl/flags/declare.h>
-#include <cstdint>
 #include <string>
 #include <string_view>
 
-#include "crypto/crypter_export.hpp"
-
+#include "config/config_core.hpp"
 #include "config/config_network.hpp"
 #include "config/config_tls.hpp"
 
-struct PortFlag {
-  explicit PortFlag(uint16_t p) : port(p) {}
-  operator uint16_t() const { return port; }
-  uint16_t port;
-};
-
-struct CipherMethodFlag {
-  explicit CipherMethodFlag(cipher_method m) : method(m) {}
-  cipher_method method;
-};
-
-ABSL_DECLARE_FLAG(std::string, server_host);
-ABSL_DECLARE_FLAG(std::string, server_sni);
-ABSL_DECLARE_FLAG(PortFlag, server_port);
-ABSL_DECLARE_FLAG(std::string, username);
-ABSL_DECLARE_FLAG(std::string, password);
-ABSL_DECLARE_FLAG(CipherMethodFlag, method);
-ABSL_DECLARE_FLAG(std::string, local_host);
-ABSL_DECLARE_FLAG(PortFlag, local_port);
-
-ABSL_DECLARE_FLAG(uint32_t, parallel_max);
-
-struct RateFlag {
-  explicit RateFlag(uint64_t r) : rate(r) {}
-  uint64_t rate;
-};
-ABSL_DECLARE_FLAG(RateFlag, limit_rate);  // bytes per second
-
 namespace config {
+
 bool ReadConfig();
 bool SaveConfig();
 
