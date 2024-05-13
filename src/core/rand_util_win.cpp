b6e3b8309e656920392ff2fccd40f256c6926c1b
Keeyou <keeyou-cn@outlook.com>
2024-05-13 09:57:59 +0800

    core: update rand_util_win

--- src/core/rand_util_win.cpp
+++ src/core/rand_util_win.cpp
@@ -8,33 +8,54 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <windows.h>
-
-// #define needed to link in RtlGenRandom(), a.k.a. SystemFunction036.  See the
-// "Community Additions" comment on MSDN here:
-// http://msdn.microsoft.com/en-us/library/windows/desktop/aa387694.aspx
-#define SystemFunction036 NTAPI SystemFunction036
-#ifdef COMPILER_MSVC
-#include <NTSecAPI.h>
-#else
-#include <ntsecapi.h>
-#endif
-#undef SystemFunction036
-
-// TODO: another alternative is BCryptGenRandom introduced in Windows Vista
-// https://docs.microsoft.com/en-us/windows/win32/api/bcrypt/nf-bcrypt-bcryptgenrandom
-
 #include <algorithm>
 #include <limits>
 
 #include <build/build_config.h>
 #include "core/logging.hpp"
 
+// Prototype for ProcessPrng.
+// See: https://learn.microsoft.com/en-us/windows/win32/seccng/processprng
+extern "C" {
+BOOL WINAPI ProcessPrng(PBYTE pbData, SIZE_T cbData);
+}
+
+namespace {
+
+// wine's trick
+decltype(&ProcessPrng) GetProcessPrngFallback(void) {
+  HMODULE hmod = LoadLibraryW(L"advapi32");
+  CHECK(hmod);
+  auto process_prng_fn =
+      reinterpret_cast<decltype(&ProcessPrng)>(reinterpret_cast<void*>(GetProcAddress(hmod, "SystemFunction036")));
+  CHECK(process_prng_fn);
+  return process_prng_fn;
+}
+
+decltype(&ProcessPrng) GetProcessPrng() {
+  HMODULE hmod = LoadLibraryW(L"bcryptprimitives");
+  if (hmod == nullptr) {
+    return GetProcessPrngFallback();
+  }
+  auto process_prng_fn =
+      reinterpret_cast<decltype(&ProcessPrng)>(reinterpret_cast<void*>(GetProcAddress(hmod, "ProcessPrng")));
+  if (process_prng_fn == nullptr) {
+    // Possible on Windows 7 SP1, need fallback as well
+    return GetProcessPrngFallback();
+  }
+  return process_prng_fn;
+}
+
+}  // namespace
+
 void RandBytes(void* output, size_t output_length) {
-  char* output_ptr = static_cast<char*>(output);
+  BYTE* output_ptr = static_cast<BYTE*>(output);
   while (output_length > 0) {
+    static decltype(&ProcessPrng) process_prng_fn = GetProcessPrng();
     const ULONG output_bytes_this_pass =
         static_cast<ULONG>(std::min(output_length, static_cast<size_t>(std::numeric_limits<ULONG>::max())));
-    const bool success = RtlGenRandom(output_ptr, output_bytes_this_pass) != FALSE;
+    const BOOL success = process_prng_fn(output_ptr, output_bytes_this_pass);
+    // ProcessPrng is documented to always return TRUE.
     CHECK(success);
     output_length -= output_bytes_this_pass;
     output_ptr += output_bytes_this_pass;
