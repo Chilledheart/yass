70630912019fb592bd19538641b168c79d07cc71
Keeyou <keeyou-cn@outlook.com>
2024-09-10 10:44:21 +0800

    gh actions: upload xz source tarball

--- scripts/build-src.sh
+++ scripts/build-src.sh
@@ -23,13 +23,16 @@ SUBVERSION=$SUBTAG
 # FIXME deb cannot pickup subtag correctly
 SUBVERSION=1
 
-rm -f yass-${VERSION}.tar yass-${VERSION}.tar.gz yass-${VERSION}.tar.bz2
+TARBALLS="yass-${VERSION}.tar.gz yass-${VERSION}.tar.bz2 yass-${VERSION}.tar.xz yass-${VERSION}.tar.zst yass-${VERSION}.zip"
+
+rm -f yass-${VERSION}.tar ${TARBALLS}
 
 /usr/bin/git ls-files --recurse-submodules | \
   tar caf yass-${VERSION}.tar --xform="s,^,yass-${VERSION}/," -T -
 
 gzip -9 --keep yass-${VERSION}.tar
 bzip2 -9 --keep yass-${VERSION}.tar
+xz -T0 -9 -f --keep yass-${VERSION}.tar
 zstd -T0 -19 -f --keep yass-${VERSION}.tar
 
 rm -f yass-${VERSION}.tar
@@ -43,16 +46,16 @@ rm -f yass-${VERSION}.tar
 
 echo "md5sum "
 echo "======================================================================"
-md5sum *.tar.gz *.tar.bz2 *.tar.zst *.zip
+md5sum ${TARBALLS}
 
 echo "sha1sum "
 echo "======================================================================"
-sha1sum *.tar.gz *.tar.bz2 *.tar.zst *.zip
+sha1sum ${TARBALLS}
 
 echo "sha256sum "
 echo "======================================================================"
-sha256sum *.tar.gz *.tar.bz2 *.tar.zst *.zip
+sha256sum ${TARBALLS}
 
 echo "sha512sum "
 echo "======================================================================"
-sha512sum *.tar.gz *.tar.bz2 *.tar.zst *.zip
+sha512sum ${TARBALLS}
