3f9a7163d4261e0885eb628dffe6597848bb3209
Keeyou <keeyou-cn@outlook.com>
2024-09-17 16:08:28 +0800

    config: remove all dead code

--- src/config/config_impl_local.hpp
+++ src/config/config_impl_local.hpp
@@ -24,12 +24,7 @@
 #include "core/utils.hpp"
 #include "core/utils_fs.hpp"
 
-#ifdef HAVE_JSONCPP
 #include <json/json.h>
-#else
-#include <nlohmann/json.hpp>
-using json = nlohmann::json;
-#endif
 
 using namespace yass;
 
@@ -60,7 +55,6 @@ class ConfigImplLocal : public ConfigImpl {
         break;
       }
       std::string_view json_content(buffer, ret);
-#ifdef HAVE_JSONCPP
       Json::CharReaderBuilder builder;
       builder["collectComments"] = false;
       Json::String err;
@@ -74,13 +68,6 @@ class ConfigImplLocal : public ConfigImpl {
         std::cerr << "bad config file: " << path_ << " content: \"" << json_content << "\"" << std::endl;
         break;
       }
-#else
-      root_ = json::parse(json_content, nullptr, false);
-      if (root_.is_discarded() || !root_.is_object()) {
-        std::cerr << "bad config file: " << path_ << " content: \"" << json_content << "\"" << std::endl;
-        break;
-      }
-#endif
       std::cerr << "loaded from config file: " << path_ << std::endl;
       return true;
     } while (false);
@@ -89,11 +76,7 @@ class ConfigImplLocal : public ConfigImpl {
       return false;
     }
 
-#ifdef HAVE_JSONCPP
     root_ = Json::objectValue;
-#else
-    root_ = json::object();
-#endif
 
     return true;
   }
@@ -109,16 +92,10 @@ class ConfigImplLocal : public ConfigImpl {
       std::cerr << "configure dir could not create: " << dir << std::endl;
       return false;
     }
-#ifdef HAVE_JSONCPP
     Json::StreamWriterBuilder builder;
     builder["commentStyle"] = "None";
     builder["indentation"] = "    ";
     const std::string json_content = Json::writeString(builder, root_);
-#else
-    // Call with defaults except in the case of UTF-8 errors which we replace
-    // invalid UTF-8 characters instead of throwing an exception.
-    const std::string json_content = root_.dump(4, ' ', false, nlohmann::detail::error_handler_t::replace);
-#endif
     if (static_cast<ssize_t>(json_content.size()) != WriteFileWithBuffer(path_, json_content)) {
       std::cerr << "failed to write to path: \"" << path_ << " with content \"" << json_content << "\"" << std::endl;
       return false;
@@ -131,145 +108,79 @@ class ConfigImplLocal : public ConfigImpl {
   }
 
   bool HasKeyStringImpl(const std::string& key) override {
-#ifdef HAVE_JSONCPP
     return root_.isMember(key) && root_[key].isString();
-#else
-    return root_.contains(key) && root_[key].is_string();
-#endif
   }
 
   bool HasKeyBoolImpl(const std::string& key) override {
-#ifdef HAVE_JSONCPP
     return root_.isMember(key) && root_[key].isBool();
-#else
-    return root_.contains(key) && root_[key].is_boolean();
-#endif
   }
 
   bool HasKeyUint32Impl(const std::string& key) override {
-#ifdef HAVE_JSONCPP
     return root_.isMember(key) && root_[key].isUInt();
-#else
-    return root_.contains(key) && root_[key].is_number_unsigned() && root_[key].is_number_integer();
-#endif
   }
 
   bool HasKeyUint64Impl(const std::string& key) override {
-#ifdef HAVE_JSONCPP
     return root_.isMember(key) && root_[key].isUInt64();
-#else
-    return root_.contains(key) && root_[key].is_number_unsigned() && root_[key].is_number_integer();
-#endif
   }
 
   bool HasKeyInt32Impl(const std::string& key) override {
-#ifdef HAVE_JSONCPP
     return root_.isMember(key) && root_[key].isInt();
-#else
-    return root_.contains(key) && root_[key].is_number_integer();
-#endif
   }
 
   bool HasKeyInt64Impl(const std::string& key) override {
-#ifdef HAVE_JSONCPP
     return root_.isMember(key) && root_[key].isInt64();
-#else
-    return root_.contains(key) && root_[key].is_number_integer();
-#endif
   }
 
   bool ReadImpl(const std::string& key, std::string* value) override {
-#ifdef HAVE_JSONCPP
     if (root_.isMember(key) && root_[key].isString()) {
       *value = root_[key].asString();
       return true;
     }
-#else
-    if (root_.contains(key) && root_[key].is_string()) {
-      *value = root_[key].get<std::string>();
-      return true;
-    }
-#endif
     std::cerr << "bad field: " << key << std::endl;
     return false;
   }
 
   bool ReadImpl(const std::string& key, bool* value) override {
-#ifdef HAVE_JSONCPP
     if (root_.isMember(key) && root_[key].isBool()) {
       *value = root_[key].asBool();
       return true;
     }
-#else
-    if (root_.contains(key) && root_[key].is_boolean()) {
-      *value = root_[key].get<bool>();
-      return true;
-    }
-#endif
     std::cerr << "bad field: " << key << std::endl;
     return false;
   }
 
   bool ReadImpl(const std::string& key, uint32_t* value) override {
-#ifdef HAVE_JSONCPP
     if (root_.isMember(key) && root_[key].isUInt()) {
       *value = root_[key].asUInt();
       return true;
     }
-#else
-    if (root_.contains(key) && root_[key].is_number_unsigned() && root_[key].is_number_integer()) {
-      *value = root_[key].get<uint32_t>();
-      return true;
-    }
-#endif
     std::cerr << "bad field: " << key << std::endl;
     return false;
   }
 
   bool ReadImpl(const std::string& key, int32_t* value) override {
-#ifdef HAVE_JSONCPP
     if (root_.isMember(key) && root_[key].isInt()) {
       *value = root_[key].asInt();
       return true;
     }
-#else
-    if (root_.contains(key) && root_[key].is_number_integer()) {
-      *value = root_[key].get<int32_t>();
-      return true;
-    }
-#endif
     std::cerr << "bad field: " << key << std::endl;
     return false;
   }
 
   bool ReadImpl(const std::string& key, uint64_t* value) override {
-#ifdef HAVE_JSONCPP
     if (root_.isMember(key) && root_[key].isUInt64()) {
       *value = root_[key].asUInt64();
       return true;
     }
-#else
-    if (root_.contains(key) && root_[key].is_number_unsigned() && root_[key].is_number_integer()) {
-      *value = root_[key].get<uint64_t>();
-      return true;
-    }
-#endif
     std::cerr << "bad field: " << key << std::endl;
     return false;
   }
 
   bool ReadImpl(const std::string& key, int64_t* value) override {
-#ifdef HAVE_JSONCPP
     if (root_.isMember(key) && root_[key].isInt64()) {
       *value = root_[key].asInt64();
       return true;
     }
-#else
-    if (root_.contains(key) && root_[key].is_number_integer()) {
-      *value = root_[key].get<int64_t>();
-      return true;
-    }
-#endif
     std::cerr << "bad field: " << key << std::endl;
     return false;
   }
@@ -305,26 +216,13 @@ class ConfigImplLocal : public ConfigImpl {
   }
 
   bool DeleteImpl(const std::string& key) override {
-#ifdef HAVE_JSONCPP
     Json::Value got;
     return root_.removeMember(key, &got);
-#else
-    auto iter = root_.find(key);
-    if (iter != root_.end()) {
-      root_.erase(iter);
-      return true;
-    }
-    return false;
-#endif
   }
 
  private:
   std::string path_;
-#ifdef HAVE_JSONCPP
   Json::Value root_;
-#else
-  json root_;
-#endif
 };
 
 }  // namespace config
