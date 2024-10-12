ac065aa05100dc2c58231937d7a52b4fe90e9bb1
Keeyou <keeyou-cn@outlook.com>
2024-10-12 18:42:55 +0800

    harmony: update next.sh

--- scripts/setup-harmony-next.sh
+++ scripts/setup-harmony-next.sh
@@ -10,12 +10,6 @@ echo "Checking Java..."
 java -version
 echo "Done"
 
-echo "Checking hvigor..."
-pushd harmony
-hvigorw -v
-popd
-echo "Done"
-
 echo "Checking npm configuration..."
 npm config set registry=https://repo.huaweicloud.com/repository/npm/
 npm config set @ohos:registry=https://repo.harmonyos.com/npm/
@@ -25,6 +19,12 @@ echo "Checking Ohpm configration..."
 ohpm --version
 ohpm config set registry https://ohpm.openharmony.cn/ohpm/
 ohpm config set strict_ssl true
+echo "Done"
+
+echo "Checking hvigor..."
+pushd harmony
+hvigorw -v
+popd
 echo "Done"
 
 echo "Checking Ohpm dependencies..."
