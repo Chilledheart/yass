88456d6ecdb9dde2079e8dc643a34fdca38dcbf2
Keeyou <keeyou-cn@outlook.com>
2024-10-18 10:04:58 +0800

    rust: bump to 1.82.0

--- scripts/build-rust.sh
+++ scripts/build-rust.sh
@@ -6,8 +6,8 @@ cd $PWD/..
 
 DEFAULT_TARGET=$(rustc -vV | sed -n 's|host: ||p')
 
-RUST_VER=1.81.0
-CARGO_VER=0.82.0
+RUST_VER=1.82.0
+CARGO_VER=0.83.0
 
 # https://github.com/Homebrew/homebrew-core/blob/master/Formula/r/rust.rb
 curl -L -O -C - https://static.rust-lang.org/dist/rustc-$RUST_VER-src.tar.xz
@@ -24,7 +24,7 @@ cat > rustc-$RUST_VER-src/config.toml.in << EOF
 
 profile = "compiler"
 # latest change id in src/bootstrap/src/utils/change_tracker.rs
-change-id = 127866
+change-id = 129295
 
 [build]
 # see https://github.com/llvm/llvm-project/issues/60115
